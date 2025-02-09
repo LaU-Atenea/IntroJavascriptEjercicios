76d85948bb1296c43087bd649b8f63e2c549bab0
Laura <lapca20@gmail.com>
2025-02-09 18:33:58 +0100

    Subo el ejercicio 2 con el código arreglado, y la explicación del bug que había

--- bug.js
+++ bug.js
@@ -1,3 +1,26 @@
-// Prueba de que el segundo ejercicio se ve bien
+/* Explicación y corrección del bug en el segundo ejercicio:
+El problema de este código está en un fallo en la estructura del for, en concreto en la parte que pone i <= numeros.length.
+Para que solo se sumen los números dentro del array, hay que eliminar ese = y poner: i < numeros.length.
+Sino, al comprobar el resultado con un console.log(promedioNumeros), nos da un NaN.
+Tras eliminar ese = sobrante, ya nos aparece en la consola el promedio correcto que es 3.
+A continuación puede verse el código arreglado y que funciona correctamente:
+*/
 
-console.log(02);
+
+const calcularPromedio = (numeros) => {
+    let sumaTotal = 0;
+
+    for (let i = 0; i < numeros.length; i++) {
+        sumaTotal += numeros [i];
+    }
+
+    const promedio = sumaTotal / numeros.length;
+
+    return promedio;
+};
+
+const listaNumeros = [1, 2, 3, 4, 5];
+
+const promedioNumeros = calcularPromedio(listaNumeros);
+
+console.log(promedioNumeros);
